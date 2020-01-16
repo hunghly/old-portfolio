23a347fde01a618ffbce9514cbfacf5d811e96c5
Hung Ly <hunghly@gmail.com>
2020-01-15 22:27:22 -0600

    fixed positioning for left and right menus, need to work on bottom (contact) menu, photos, and giant modal

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
     "use strict";
     $(document).ready(() => {
         let leftMenuContainerEl = $('.left-menu-container');
+        let rightMenuContainerEl = $('.right-menu-container');
 
         let switchLeftArrow = (element) => {
             if ($(element).hasClass('fa-chevron-circle-right')) {
@@ -13,7 +14,20 @@
             }
         };
 
+        let switchRightArrow = (element) => {
+            if ($(element).hasClass('fa-chevron-circle-left')) {
+                $(element).removeClass('fa-chevron-circle-left');
+                $(element).addClass('fa-chevron-circle-right');
+            } else {
+                $(element).removeClass('fa-chevron-circle-right');
+                $(element).addClass('fa-chevron-circle-left');
+            }
+        };
+
         $('.nav-item-left').children('h3').click(function () {
+            $(this).next().slideToggle();
+        });
+        $('.nav-item-right').children('h3').click(function () {
             $(this).next().slideToggle();
         });
 
@@ -23,6 +37,14 @@
             console.log($('.left-arrow'));
             $('.left-arrow').each(function () {
                 switchLeftArrow(this);
+            });
+        });
+        $('#right-menu-header').click(function () {
+            $(rightMenuContainerEl).toggleClass('right-menu-container-open');
+            $(rightMenuContainerEl).toggleClass('right-menu-container-close');
+            console.log($('.right-arrow'));
+            $('.right-arrow').each(function () {
+                switchRightArrow(this);
             });
         });
 
